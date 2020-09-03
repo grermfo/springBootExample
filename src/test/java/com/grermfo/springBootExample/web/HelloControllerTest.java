b802bfd331223c161e0d5670770c725133283ab3
hj.an <p90430@shinsegae.com>
2020-09-03 16:15:08 +0900

    HelloController - TEST[junit]  추가

---
+++ src/test/java/com/grermfo/springBootExample/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.grermfo.springBootExample.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers =  HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void returnHello() throws Exception {
+        String helloWorld = "Hello! World";
+        mvc.perform(get("/helloWorld"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(helloWorld));
+    }
+}
