7677acda0f8cbae672278ddb89e3ffb5813de56a
hj.an <p90430@shinsegae.com>
2020-09-04 17:25:02 +0900

    lombok Test 추가

---
+++ src/test/java/com/grermfo/springBootExample/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.grermfo.springBootExample.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokFunctionTest() {
+        String name = "grermfo";
+        int amount = 30;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
