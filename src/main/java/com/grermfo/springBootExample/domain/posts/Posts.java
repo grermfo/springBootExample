daacb2ac0b6ff6e74708de91b71814e11377ce0b
hj.an <p90430@shinsegae.com>
2020-09-15 17:10:32 +0900

    test 오타 수정

--- src/main/java/com/grermfo/springBootExample/domain/posts/Posts.java
+++ src/main/java/com/grermfo/springBootExample/domain/posts/Posts.java
@@ -30,7 +30,7 @@ public class Posts {
         this.author = author;
     }
 
-    public void update(String title, String cotent) {
+    public void update(String title, String content) {
         this.title = title;
         this.content = content;
 
