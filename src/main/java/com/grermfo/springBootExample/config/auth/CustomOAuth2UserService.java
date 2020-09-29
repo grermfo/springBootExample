7b8a522390c12a435f6b249497bd4b2b7ed9bb38
grermfo <grermfo@gmail.com>
2020-09-29 22:28:18 +0900

    스프링 시큐리티 사용자 정보 서비스 추가

--- src/main/java/com/grermfo/springBootExample/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/grermfo/springBootExample/config/auth/CustomOAuth2UserService.java
@@ -1,8 +1,10 @@
 package com.grermfo.springBootExample.config.auth;
 
 import com.grermfo.springBootExample.config.auth.dto.OAuthAttributes;
+import com.grermfo.springBootExample.config.auth.dto.SessionUser;
 import com.grermfo.springBootExample.domain.user.User;
 import com.grermfo.springBootExample.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -10,10 +12,13 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
 
+@RequiredArgsConstructor
+@Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
     private final HttpSession httpSession;
@@ -26,12 +31,20 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
       String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
                                                 .getUserInfoEndpoint().getUserNameAttributeName();
 
-      OAuthAttributes attributes= OAuthAtttributes.of(registrationId, userNameAttributeName,oAuth2User.getAttributes());
+      OAuthAttributes attributes= OAuthAttributes.of(registrationId, userNameAttributeName,oAuth2User.getAttributes());
         User user = saveOrUpdate(attributes);
         httpSession.setAttribute("user", new SessionUser(user));
         return  new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
                                       attributes.getAttributes(),
                                       attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                                  .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                                  .orElse(attributes.toEntity());
+        return userRepository.save(user);
+
     }
 }
 
